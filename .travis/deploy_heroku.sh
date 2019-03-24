637d8e843d398f8f93bd1fdd76d6fc852f3cc4fa
James Dellaguardia <JAMES.DELLAGUARDIA@LIBERTYMUTUAL.COM>
2019-03-24 12:48:44 -0400

    release to heroku

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -2,3 +2,4 @@
 heroku container:login
 echo "$HEROKU_API_KEY" | docker login --username=_ --password-stdin registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
+heroku container:release web --app $HEROKU_APP_NAME
